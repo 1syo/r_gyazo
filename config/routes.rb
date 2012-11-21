b86d842fa2b8e6782ee83d9b3368b9ace25dcacf
TAKAHASHI Kazunari <takahashi@1syo.net>
2012-11-21 15:27:41 +0900

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 RGyazo::Application.routes.draw do
-  post "create" => "images#create", as: :images
+  post "upload.cgi" => "images#create", as: :images
   match ":name.:format", :to => "images#show", as: :image
-  get "/404", :to => "welcom#error_404", as: :error_404
-  get "/403", :to => "welcom#error_403", as: :error_403
-  root :to => "welcom#index"
+  get "404", :to => "index#error_404", as: :error_404
+  get "403", :to => "index#error_403", as: :error_403
+  root :to => "index#index"
 end
