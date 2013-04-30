fd6ca72edbf693810abd4a572e5436976aead68c
TAKAHASHI Kazunari <takahashi@1syo.net>
2013-04-30 20:46:02 +0900

    rename

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RGyazo::Application.config.secret_token = '052e769ea60e5a25bb411667181b6650cb6ccad80bca38fc2625fb75a9a64eca21e3ad7a3992e442d0b15323ef9e2a80ed6a9abb42ef9eeb3b8efc7cab133bed'
+RGyazo::Application.config.secret_key_base = '052e769ea60e5a25bb411667181b6650cb6ccad80bca38fc2625fb75a9a64eca21e3ad7a3992e442d0b15323ef9e2a80ed6a9abb42ef9eeb3b8efc7cab133bed'
