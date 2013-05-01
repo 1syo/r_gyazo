fb6326345e854dc9ccc6f2dca833dc0aefc6a158
TAKAHASHI Kazunari <takahashi@1syo.net>
2013-05-01 20:03:54 +0900

    refactor database.yaml

--- db/schema.rb
+++ db/schema.rb
@@ -9,17 +9,17 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121117072410) do
+ActiveRecord::Schema.define(version: 20121117072410) do
 
-  create_table "images", :force => true do |t|
-    t.string   "name",       :null => false
-    t.binary   "data",       :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "images", force: true do |t|
+    t.string   "name",       null: false
+    t.binary   "data",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "images", ["name"], :name => "index_images_on_name", :unique => true
+  add_index "images", ["name"], name: "index_images_on_name", unique: true
 
 end
