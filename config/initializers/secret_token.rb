13ffcadcaab6423395ee4214d58bdb6b607493f0
TAKAHASHI Kazunari <takahashi@1syo.net>
2013-06-14 14:43:17 +0900

    bundle exec rake rails:update

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RGyazo::Application.config.secret_key_base = '052e769ea60e5a25bb411667181b6650cb6ccad80bca38fc2625fb75a9a64eca21e3ad7a3992e442d0b15323ef9e2a80ed6a9abb42ef9eeb3b8efc7cab133bed'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RGyazo::Application.config.secret_key_base = '56f5dd0ec95eaf5de27dd005a9ae056e3c8b7e15492e5ffa3a6af2ed80abf2c6ee4947d140e195d8bbd81c258ca778e78a3b15af3a5d13681ae17b8fbc49b1ab'
