f1e3f8e021bd477d021055c106179ec7869e9431
TAKAHASHI Kazunari <takahashi@1syo.net>
2013-06-14 15:58:02 +0900

    rake db:migrate

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,4 +1,5 @@
 class AdminUser < ActiveRecord::Base
+  attr_accessible :email, :password, :password_confirmation
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
