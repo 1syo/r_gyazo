69a98390d84b979279028169b1dc3abcbbbbd96d
TAKAHASHI Kazunari <takahashi@1syo.net>
2013-06-14 15:55:04 +0900

    rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 RGyazo::Application.routes.draw do
-  resources :admin, only: [:index, :destroy]
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  #resources :admin, only: [:index, :destroy]
   post "upload.cgi" => "images#create", as: :images
   get "404", :to => "index#error_404", as: :error_404
   get "403", :to => "index#error_403", as: :error_403
